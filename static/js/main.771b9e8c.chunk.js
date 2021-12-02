727b4f184da6a4f06e82763bd9d06535f7951d81
zaktitov <zaktitov@gmail.com>
2021-12-02 14:36:42 +0100

    Updates

--- static/js/main.771b9e8c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],{11:function(t,n,e){"use strict";e.r(n);var c=e(1),o=e.n(c),s=e(3),r=e.n(s),i=(e(8),e(9),e(0));var a=function(){return Object(i.jsx)("div",{className:"App"})};r.a.render(Object(i.jsx)(o.a.StrictMode,{children:Object(i.jsx)(a,{})}),document.getElementById("root"))},8:function(t,n,e){},9:function(t,n,e){}},[[11,1,2]]]);
-//# sourceMappingURL=main.771b9e8c.chunk.js.map
